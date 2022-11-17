cbc0c95554df2231549ad5ceb60f986738c1b170
Pedro Esposito <pedro.esposito99@gmail.com>
2022-11-17 13:24:51 -0300

    Primer commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./NavAndFooter/Navbar"
+import Footer from "./NavAndFooter/Footer"
+export default function Home(){
+  return(
+    <div>
+      <Navbar/>
+      <h1>HOME</h1>
+      <Footer/>
+    </div>
+  )
+}
---
+++ src/components/NavAndFooter/Navbar.jsx
@@ -0,0 +1,21 @@
+export default function Navbar(){
+  return(
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Legaltech Seed</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+          <a className="nav-link" href="#">Quienes somos</a>
+          <a className="nav-link" href="#">Nuestras actividades</a>
+          <a className="nav-link" href="#">Contenido</a>
+          <a className="nav-link" href="#">Se parte</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  )
+}
