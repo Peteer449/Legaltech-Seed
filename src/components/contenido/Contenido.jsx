40f536dcdb1aced3789427e87ff581b32ed137e9
francisco-danelon <frandane.eth@gmail.com>
2022-12-28 15:49:03 -0300

    Nuestras Actividades

--- src/components/contenido/Contenido.jsx
+++ src/components/contenido/Contenido.jsx
@@ -1,7 +1,161 @@
-export default function Contenido(){
-  return(
-    <div>
-      Contenido
+import "./Contenido.css";
+
+const Contenido = () => {
+  return (
+    <div className="contenido">
+      <div className="box-3" />
+      <div className="box-31" />
+      <div className="box-32" />
+      <div className="box-33" />
+      <div className="box-34" />
+      <div className="descubr-todo-el-material-que">
+        <p className="descubr-todo-el">
+          ¡Descubrí todo el material que producimos y las actividades en las que
+          trabajamos!
+        </p>
+      </div>
+      <div className="proyectos">
+        <p className="descubr-todo-el">Proyectos</p>
+      </div>
+      <div className="investigacin">
+        <p className="descubr-todo-el">Investigación</p>
+      </div>
+      <div className="adultos-mayores">
+        <p className="descubr-todo-el">Adultos Mayores</p>
+      </div>
+      <div className="editorial">
+        <p className="descubr-todo-el">Editorial</p>
+      </div>
+      <div className="proyecto-informateados-segme">
+        <p className="descubr-todo-el">● Proyecto "Informateados"</p>
+        <p className="descubr-todo-el">
+          Segmento dedicado a la producción y difusión de artículos con
+          diferentes análisis de la relación Derecho-Tecnología-Sociedad a
+          través de un blog.
+        </p>
+        <p className="descubr-todo-el">
+          {`(link: `}
+          <a
+            className="httpslegaltechseedwixsite"
+            href="https://legaltechseed.wixsite.com/legaltechseed/informateados-1"
+            target="_blank"
+          >
+            https://legaltechseed.wixsite.com/legaltechseed/informateados-1
+          </a>{" "}
+          )
+        </p>
+        <p className="descubr-todo-el">● Proyecto "Informateados LIVE"</p>
+        <p className="descubr-todo-el">
+          Es un espacio de difusión de noticias acerca de la tecnología. Surgió
+          como una idea del semillero que se vinculó a Informateados por su
+          similitud. El ámbito de difusión de IL son las redes sociales.
+        </p>
+        <p className="descubr-todo-el">● Proyecto "Criptonizate"</p>
+      </div>
+      <div className="infopublis-son-producciones-de">
+        <p className="descubr-todo-el">
+          <span>● Infopublis</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>
+            Son producciones destinadas a las redes sociales, cuyo objetivo
+            comunicacional principal es educar a los usuarios sobre las
+            temáticas legales e informáticas que nos comprometen.
+          </span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>● Club de debates</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>
+            Encuentro una vez cada dos meses aproximadamente, donde los miembros
+            nos juntamos a charlar, pensar y debatir sobre temáticas actuales.
+          </span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>● Expertos en datos</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>{`Proyecto a largo plazo que analiza toda la normativa de datos personales (nacional e internacional). En la primera etapa comentamos la Ley de Datos Personales. En la segunda instancia, tuvimos una entrevista con gente de la Agencia de Acceso a la Información. Y en la tercera, elaboramos un exhaustivo informe al cual pueden tener acceso las personas que les interesa la temática a través del siguiente `}</span>
+          <span className="link">link</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>&nbsp;</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>● Artículos, ensayos e informes</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>
+            Muchas de nuestras producciones han sido publicadas en diferentes
+            editoriales y han participado en diferentes congresos académicos.
+          </span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>
+            ● Taller dictado por Mariana Sánchez Caparrós sobre cómo empezar a
+            escribir
+          </span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>● Comentarios de actualidad</span>
+        </p>
+      </div>
+      <div className="diccionario-tech-para-adultos">
+        <p className="descubr-todo-el">
+          <span>● Diccionario Tech para adultos mayores (y no tanto)"</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>
+            Diccionario elaborado a raíz de un trabajo colaborativo,
+            participativo y dinámico que fuimos construyendo desde Legaltech
+            Seed Derecho 4.0, como parte de nuestro aporte de Responsabilidad
+            Social hacia los Adultos Mayores en especial.
+          </span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>{`Podés descargarlo `}</span>
+          <span className="link">aquí</span>
+          <span>&nbsp;</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>{`¿Te gustaría sumar nuevos términos? Hacé click `}</span>
+          <span className="link">aquí</span>
+          <span>&nbsp;</span>
+        </p>
+        <p className="talleres-de-formacin">
+          <span>● Talleres de formación</span>
+        </p>
+      </div>
+      <div className="podcast-millennials-hackeando">
+        <p className="descubr-todo-el">
+          <span>● Podcast Millennials hackeando al Derecho</span>
+        </p>
+        <p className="descubr-todo-el">
+          <span>
+            Es un Congreso virtual organizado por Legaltech Seed dedicado al
+            abordaje interdisciplinario de las principales transformaciones y
+            sucesos ligados al Derecho y la Tecnología. Articulando la
+            participación de los miembros del semillero en sus diferentes etapas
+            de desarrollo, el evento es realizado por estudiantes, docentes y
+            profesionales comprometidos con el estudio y análisis de la 4°
+            Revolución Industrial que transitamos.
+          </span>
+        </p>
+        <p className="talleres-de-formacin">
+          <span>{`¡Escuchalo `}</span>
+          <span className="link">aquí</span>
+        </p>
+      </div>
+      <img className="circle-bg-icon" alt="" src="../circle-bg.svg" />
+      <img className="circle-bg-icon1" alt="" src="../circle-bg.svg" />
+      <img className="circle-bg-icon2" alt="" src="../circle-bg2.svg" />
+      <img className="circle-bg-icon3" alt="" src="../circle-bg2.svg" />
+      <img className="vector-icon" alt="" src="../vector-2.svg" />
+      <img className="vector-icon1" alt="" src="../vector-2.svg" />
+      <img className="vector-icon2" alt="" src="../vector-2.svg" />
     </div>
-  )
-}
+  );
+};
+
+export default Contenido;
