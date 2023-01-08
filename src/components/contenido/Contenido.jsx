0f0b42c6a1a32df0965ed07419098169ba5f24cd
francisco-danelon <frandane.eth@gmail.com>
2023-01-08 12:04:14 -0300

    Contenido y retoques en otras secciones

--- src/components/contenido/Contenido.jsx
+++ src/components/contenido/Contenido.jsx
@@ -2,159 +2,86 @@ import "./Contenido.css";
 
 const Contenido = () => {
   return (
-    <div className="contenido">
-      <div className="box-3" />
-      <div className="box-31" />
-      <div className="box-32" />
-      <div className="box-33" />
-      <div className="box-34" />
-      <div className="descubr-todo-el-material-que">
-        <p className="descubr-todo-el">
-          ¡Descubrí todo el material que producimos y las actividades en las que
-          trabajamos!
-        </p>
+    <>
+      <div id="contenido-introduccion">
+        <div id="contenido-introduccion-container" className="text-white">
+          <p id="contenido-introduccion-title" className="pt-5 pb-5">
+          ¡Descubrí todo el material que producimos y las actividades en las que trabajamos!
+          </p>
+        </div>
       </div>
-      <div className="proyectos">
-        <p className="descubr-todo-el">Proyectos</p>
+
+      {/* Proyectos */}
+
+      <div id="contenido-proyectos">
+        <div id="contenido-proyectos-container" className="text-white">
+          <p id="contenido-proyectos-title" className="pt-5 pb-5">
+            Proyectos
+          </p>
+          <ul id="contenido-proyectos-list" className="pt-4">
+            <li className="contenido-list-title">Proyecto "Informateados"</li>
+            <p className="contenido-text">Segmento dedicado a la producción y difusión de artículos con diferentes análisis de la relación Derecho-Tecnología-Sociedad a través de un blog.</p>
+            <li className="contenido-list-title">Proyecto "informateados LIVE"</li>
+            <p className="contenido-text">Es un espacio de difusión de noticias acerca de la tecnología. Surgió como una idea del semillero que se vinculó a Informateados por su similitud. El ámbito de difusión de IL son las redes sociales.</p>
+            <li className="contenido-list-title">Proyecto "Criptonizate"</li>
+          </ul>
+        </div>
       </div>
-      <div className="investigacin">
-        <p className="descubr-todo-el">Investigación</p>
+
+    {/* Investigacion */}
+
+    <div id="contenido-investigacion">
+        <div id="contenido-investigacion-container" className="text-white">
+          <p id="contenido-investigacion-title" className="pt-5 pb-5">
+          Investigación
+          </p>
+          <ul id="contenido-investigacion-list" className="pt-4">
+            <li className="contenido-list-title">Infopublis</li>
+            <p className="contenido-text">Son producciones destinadas a las redes sociales, cuyo objetivo comunicacional principal es educar a los usuarios sobre las temáticas legales e informáticas que nos comprometen.</p>
+            <li className="contenido-list-title">Club de debates</li>
+            <p className="contenido-text">Encuentro una vez cada dos meses aproximadamente, donde los miembros nos juntamos a charlar, pensar y debatir sobre temáticas actuales.</p>
+            <li className="contenido-list-title">Expertos en datos</li>
+            <p className="contenido-text">Proyecto a largo plazo que analiza toda la normativa de datos personales (nacional e internacional). En la primera etapa comentamos la Ley de Datos Personales. En la segunda instancia, tuvimos una entrevista con gente de la Agencia de Acceso a la Información. Y en la tercera, elaboramos un exhaustivo informe al cual pueden tener acceso las personas que les interesa la temática a través del siguiente <a href="" className="black-link">link</a></p>
+            <li className="contenido-list-title">Artículos, ensayos e informes</li>
+            <p className="contenido-text">Muchas de nuestras producciones han sido publicadas en diferentes editoriales y han participado en diferentes congresos académicos.</p>
+            <li className="contenido-list-title">Taller dictado por Mariana Sánchez Caparrós sobre cómo empezar a escribir</li>
+            <li className="contenido-list-title">Comentarios de actualidad</li>
+          </ul>
+        </div>
       </div>
-      <div className="adultos-mayores">
-        <p className="descubr-todo-el">Adultos Mayores</p>
+
+    {/* Adultos Mayores */}
+
+    <div id="contenido-mayores">
+        <div id="contenido-mayores-container" className="text-white">
+          <p id="contenido-mayores-title" className="pt-5 pb-5">
+            Adultos Mayores
+          </p>
+          <ul id="contenido-mayores-list" className="pt-4">
+            <li className="contenido-list-title">Diccionario Tech para adultos mayores (y no tanto)"</li>
+            <p className="contenido-text">Diccionario elaborado a raíz de un trabajo colaborativo, participativo y dinámico que fuimos construyendo desde Legaltech Seed Derecho 4.0, como parte de nuestro aporte de Responsabilidad Social hacia los Adultos Mayores en especial.</p>
+            <p>Podés descargarlo <a href="" className="black-link">aquí</a></p>
+            <p>¿Te gustaría sumar nuevos términos? Hacé click <a href="" className="black-link">aquí</a></p>
+            <li className="contenido-list-title">Talleres de formación</li>
+          </ul>
+        </div>
       </div>
-      <div className="editorial">
-        <p className="descubr-todo-el">Editorial</p>
+
+    {/* Editorial */}
+
+    <div id="contenido-editorial">
+        <div id="contenido-editorial-container" className="text-white">
+          <p id="contenido-editorial-title" className="pt-5 pb-5">
+            Editorial
+          </p>
+          <ul id="contenido-editorial-list" className="pt-4">
+            <li className="contenido-list-title">Podcast Millennials hackeando al Derecho</li>
+            <p className="contenido-text">Es un Congreso virtual organizado por Legaltech Seed dedicado al abordaje interdisciplinario de las principales transformaciones y sucesos ligados al Derecho y la Tecnología. Articulando la participación de los miembros del semillero en sus diferentes etapas de desarrollo, el evento es realizado por estudiantes, docentes y profesionales comprometidos con el estudio y análisis de la 4° Revolución Industrial que transitamos.</p>
+            <p>¡Escuchalo <a href="" className="black-link">aquí</a> !</p>
+          </ul>
+        </div>
       </div>
-      <div className="proyecto-informateados-segme">
-        <p className="descubr-todo-el">● Proyecto "Informateados"</p>
-        <p className="descubr-todo-el">
-          Segmento dedicado a la producción y difusión de artículos con
-          diferentes análisis de la relación Derecho-Tecnología-Sociedad a
-          través de un blog.
-        </p>
-        <p className="descubr-todo-el">
-          {`(link: `}
-          <a
-            className="httpslegaltechseedwixsite"
-            href="https://legaltechseed.wixsite.com/legaltechseed/informateados-1"
-            target="_blank"
-          >
-            https://legaltechseed.wixsite.com/legaltechseed/informateados-1
-          </a>{" "}
-          )
-        </p>
-        <p className="descubr-todo-el">● Proyecto "Informateados LIVE"</p>
-        <p className="descubr-todo-el">
-          Es un espacio de difusión de noticias acerca de la tecnología. Surgió
-          como una idea del semillero que se vinculó a Informateados por su
-          similitud. El ámbito de difusión de IL son las redes sociales.
-        </p>
-        <p className="descubr-todo-el">● Proyecto "Criptonizate"</p>
-      </div>
-      <div className="infopublis-son-producciones-de">
-        <p className="descubr-todo-el">
-          <span>● Infopublis</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>
-            Son producciones destinadas a las redes sociales, cuyo objetivo
-            comunicacional principal es educar a los usuarios sobre las
-            temáticas legales e informáticas que nos comprometen.
-          </span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>● Club de debates</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>
-            Encuentro una vez cada dos meses aproximadamente, donde los miembros
-            nos juntamos a charlar, pensar y debatir sobre temáticas actuales.
-          </span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>● Expertos en datos</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>{`Proyecto a largo plazo que analiza toda la normativa de datos personales (nacional e internacional). En la primera etapa comentamos la Ley de Datos Personales. En la segunda instancia, tuvimos una entrevista con gente de la Agencia de Acceso a la Información. Y en la tercera, elaboramos un exhaustivo informe al cual pueden tener acceso las personas que les interesa la temática a través del siguiente `}</span>
-          <span className="link">link</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>&nbsp;</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>● Artículos, ensayos e informes</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>
-            Muchas de nuestras producciones han sido publicadas en diferentes
-            editoriales y han participado en diferentes congresos académicos.
-          </span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>
-            ● Taller dictado por Mariana Sánchez Caparrós sobre cómo empezar a
-            escribir
-          </span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>● Comentarios de actualidad</span>
-        </p>
-      </div>
-      <div className="diccionario-tech-para-adultos">
-        <p className="descubr-todo-el">
-          <span>● Diccionario Tech para adultos mayores (y no tanto)"</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>
-            Diccionario elaborado a raíz de un trabajo colaborativo,
-            participativo y dinámico que fuimos construyendo desde Legaltech
-            Seed Derecho 4.0, como parte de nuestro aporte de Responsabilidad
-            Social hacia los Adultos Mayores en especial.
-          </span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>{`Podés descargarlo `}</span>
-          <span className="link">aquí</span>
-          <span>&nbsp;</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>{`¿Te gustaría sumar nuevos términos? Hacé click `}</span>
-          <span className="link">aquí</span>
-          <span>&nbsp;</span>
-        </p>
-        <p className="talleres-de-formacin">
-          <span>● Talleres de formación</span>
-        </p>
-      </div>
-      <div className="podcast-millennials-hackeando">
-        <p className="descubr-todo-el">
-          <span>● Podcast Millennials hackeando al Derecho</span>
-        </p>
-        <p className="descubr-todo-el">
-          <span>
-            Es un Congreso virtual organizado por Legaltech Seed dedicado al
-            abordaje interdisciplinario de las principales transformaciones y
-            sucesos ligados al Derecho y la Tecnología. Articulando la
-            participación de los miembros del semillero en sus diferentes etapas
-            de desarrollo, el evento es realizado por estudiantes, docentes y
-            profesionales comprometidos con el estudio y análisis de la 4°
-            Revolución Industrial que transitamos.
-          </span>
-        </p>
-        <p className="talleres-de-formacin">
-          <span>{`¡Escuchalo `}</span>
-          <span className="link">aquí</span>
-        </p>
-      </div>
-      <img className="circle-bg-icon" alt="" src="../circle-bg.svg" />
-      <img className="circle-bg-icon1" alt="" src="../circle-bg.svg" />
-      <img className="circle-bg-icon2" alt="" src="../circle-bg2.svg" />
-      <img className="circle-bg-icon3" alt="" src="../circle-bg2.svg" />
-      <img className="vector-icon" alt="" src="../vector-2.svg" />
-      <img className="vector-icon1" alt="" src="../vector-2.svg" />
-      <img className="vector-icon2" alt="" src="../vector-2.svg" />
-    </div>
+    </>
   );
 };
 
