436b699e512544bb14f0887758fe57bb23e25d8e
francisco-danelon <frandane.eth@gmail.com>
2023-02-14 13:30:19 -0500

    agrego texto en nuestrasActividades + links en contenido

--- src/components/nuestrasActividades/nuestrasActividades.jsx
+++ src/components/nuestrasActividades/nuestrasActividades.jsx
@@ -26,17 +26,20 @@ export default function NuestrasActividades() {
             Proyectos
           </p>
           <p id="proyectos-text">
-            Germinación de proyectos y movimiento de ideas creativas. <br />{" "}
-            Nuestras producciones están pensadas para su <br /> presentación
-            abierta y de libre acceso para cualquier <br /> usuario de internet.
+            Germinación de proyectos y movimiento de ideas creativas. <br />
+            Nuestras producciones están pensadas para su presentación <br />
+            abierta y de libre acceso para cualquier usuario de internet.
           </p>
           <p id="proyectos-text" className="pt-4">
             Actividades activas
           </p>
           <ul id="proyectos-list" className="pt-4">
             <li>Proyecto "Informateados"</li>
+            <p className="desc-li">Segmento dedicado a la producción y difusión de artículos con diferentes análisis de la relación Derecho-Tecnología-Sociedad a través de un blog.</p>
             <li>Proyecto "informateados LIVE"</li>
-            <li>Proyecto "Criptonizate"</li>
+            <p className="desc-li">Es un espacio de difusión de noticias acerca de la tecnología. Surgió como una idea del semillero que se vinculó a Informateados por su similitud. El ámbito de difusión de IL son las redes sociales.</p>
+            <li>Proyecto "Internet Segura"</li>
+            <p className="desc-li">En conjunto con la Dirección de Juventudes de la provincia de Mendoza,  participamos en el equipo de “Internet Segura”. Este proyecto tiene como objetivo capacitar y concientizar a jóvenes en edad escolar sobre el buen uso del entorno digital, concientizamos sobre los peligros y buenas prácticas en los entornos digitales.</p>
           </ul>
         </div>
       </div>
@@ -55,9 +58,12 @@ export default function NuestrasActividades() {
             Proyectos activos
           </p>
           <ul id="investigacion-list" className="pt-4">
-            <li>Infopublis</li>
             <li>Club de debates</li>
+            <p className="desc-li-whiteBg">Encuentro una vez cada dos meses aproximadamente, donde los miembros nos juntamos a charlar, pensar y debatir sobre temáticas actuales.</p>
             <li>Expertos en datos</li>
+            <p className="desc-li-whiteBg">Proyecto a largo plazo que analiza toda la normativa de datos personales (nacional e internacional).  En la primera etapa comentamos la Ley de Datos Personales. En la segunda instancia, tuvimos una entrevista con gente de la Agencia de Acceso a la Información. Y en la tercera, elaboramos un exhaustivo informe al cual pueden tener acceso las personas que les interesa la temática.</p>
+            <li>Infopublis</li>
+            <p className="desc-li-whiteBg">Son producciones destinadas a las redes sociales, cuyo objetivo comunicacional principal es educar a los usuarios sobre las temáticas legales e informáticas que nos comprometen.</p>
           </ul>
         </div>
       </div>
@@ -76,6 +82,7 @@ export default function NuestrasActividades() {
           </p>
           <ul id="adultos-mayores-list" className="pt-4">
             <li> "Diccionario Tech para adultos mayores (y no tanto)"</li>
+            <p className="desc-li"> Diccionario elaborado a raíz de un trabajo colaborativo, participativo y dinámico que fuimos construyendo desde Legaltech Seed Derecho 4.0, como parte de nuestro aporte de Responsabilidad Social hacia los Adultos Mayores en especial.</p>
             <li> Talleres de formación</li>
           </ul>
         </div>
